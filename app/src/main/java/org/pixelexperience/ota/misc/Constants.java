09457286e69010c1e2313ef295ed59a012be1825
silent27121984 <sergey27121984@gmail.com>
2018-12-21 13:53:56 +0300

    new ota url1

--- app/src/main/java/org/pixelexperience/ota/misc/Constants.java
+++ app/src/main/java/org/pixelexperience/ota/misc/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     // Build vars
     public static final String CURRENT_DEVICE_NAME = "org.pixelexperience.device";
     public static final String CURRENT_VERSION = "org.pixelexperience.version";
-    public static final String OTA_URL = "http://pe-go.ru/ota/silent27/pe/santoni/%s";
+    public static final String OTA_URL = "http://pe-go.ru/ota/silent27/pe/%s";
     public static final String OTA_VERSION_CODE = "org.pixelexperience.ota.version_code";
     // Expressions
     public static final String FILENAME_DATE_FORMAT = "yyyyMMdd-Hm";
